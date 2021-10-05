1b383159d50a3f79bdcb57c49842eefe08b0e542
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-05 11:47:05 +0200

    creation du toggle sidebar navbar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,15 @@
 // == Import
 import Navbar from '../Navbar';
-import reactLogo from './react-logo.svg';
 import './styles.scss';
 import {BrowserRouter as Router} from 'react-router-dom'
-import Sidebar from '../Sidebar';
+import Home from '../../pages';
 
 // == Composant
 const App = () => {
 
   return (
     <Router>
-      <Sidebar />
-      <Navbar />
+      <Home />
     </Router>
   );
 }
