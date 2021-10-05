5b1d5d4c8d9add4eb0b6660d3604dc277914d92e
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-05 22:39:54 +0200

    debut debeugage video mais lecture encore impossible

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,15 +1,56 @@
+import { useState } from 'react';
+import Video from '../../assets/vid/wave.mp4';
+import { Button } from '../ButtonElement';
 import {
   HeroContainer,
   HeroBg,
-  // VideoBg,
+  VideoBg,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+  HeroContent,
 } from './HeroElements';
 
-const index = () => (
-  <HeroContainer id="home">
-    <HeroBg>
-      {/* <VideoBg autoplay loop muted src={} type="video/mp4" /> */}
-    </HeroBg>
-  </HeroContainer>
-);
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
 
-export default index;
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoplay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Lorem ipsum dolor sit amet</HeroH1>
+        <HeroP>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum."
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
