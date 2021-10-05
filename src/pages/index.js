917880962561f928f47978b51f2bf7959d837891
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-05 11:38:31 +0200

    début mise en place de la page Home

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Navbar from '../components/Sidebar'
+
+const Home = () => {
+  return (
+    <>
+      <Sidebar />
+      <Navbar />
+    </>
+  )
+}
+
+export default Home
