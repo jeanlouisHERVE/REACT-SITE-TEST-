4cd64040b899b675c89b34530bdb1eccd550cf4c
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-05 11:36:47 +0200

    finalisation sidebar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,14 @@ import Navbar from '../Navbar';
 import reactLogo from './react-logo.svg';
 import './styles.scss';
 import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from '../Sidebar';
 
 // == Composant
 const App = () => {
 
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
